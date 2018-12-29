76039664fd379d15e189cc1448b6182175eb9082
guyof@wincs.cs.bgu.ac.il <ziv@haroety.com>
2018-12-29 20:42:16 +0200

    end of saturday for ziv

--- spl-net/src/main/java/bgu/spl/net/srv/NonBlockingConnectionHandler.java
+++ spl-net/src/main/java/bgu/spl/net/srv/NonBlockingConnectionHandler.java
@@ -53,8 +53,7 @@ public class NonBlockingConnectionHandler<T> implements ConnectionHandler<T> {
                             protocol.process(nextMessage);
                         /*    T response = protocol.process(nextMessage);
                             if (response != null) {
-                                writeQueue.add(ByteBuffer.wrap(encdec.encode(response)));
-                                reactor.updateInterestedOps(chan, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+
                             } */
                         }
                     }
@@ -118,5 +117,8 @@ public class NonBlockingConnectionHandler<T> implements ConnectionHandler<T> {
         BUFFER_POOL.add(buff);
     }
 
-
+    public void send(T msg){
+        writeQueue.add(ByteBuffer.wrap(encdec.encode(msg)));
+        reactor.updateInterestedOps(chan, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+    }
 }
